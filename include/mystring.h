ca859f3ad1c1f49bcccc2341ebbcf014cf8457ca
TANKIST256 <125738490+tankist256@users.noreply.github.com>
2025-12-03 04:21:24 +0200

    Delete include directory

--- include/mystring.h
+++
@@ -1,9 +0,0 @@
-#ifndef MYSTRING_H
-#define MYSTRING_H
-
-int myStrcmp(const char *s1, const char *s2);
-int myStrlen(const char *s);
-char *myStrcpy(char *dest, const char *src);
-char *myStrdup(const char *s);
-
-#endif
